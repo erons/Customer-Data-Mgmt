a9dc5176c4d77e0104182301696c0360323ca901
erons <ifestie@gmail.com>
2018-06-13 16:56:56 +0200

    Changed Position of Add Customer and About

--- mcustomer/src/main.js
+++ mcustomer/src/main.js
@@ -36,24 +36,18 @@ new Vue({
   router,
   template: `
    <div id= "app">
-   <nav class="navbar navbar-expand-sm navbar-dark bg-dark ">
+   <nav class="navbar navbar-default ">
         <a class="navbar-brand" href="#">Customers</a>
    <div class="container">
      <div class="navbar-header">
-       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-         <span class="sr-only">Toggle navigation</span>
-         <span class="icon-bar"></span>
-         <span class="icon-bar"></span>
-         <span class="icon-bar"></span>
-       </button>
      </div>
      <div id="navbar" class="collapse navbar-collapse">
        <ul class="nav navbar-nav">
          <li><router-link to="/">Home</router-link></li>
-         <li><router-link to="/about">About</router-link></li>
+         <li><router-link to="/add">Add Customer</router-link></li>
        </ul>
        <ul class="nav navbar-nav navbar-right">
-         <li><router-link to="/add">Add Customer</router-link></li>
+         <li><router-link to="/about">About</router-link></li>
        </ul>
      </div><!--/.nav-collapse -->
    </div>
